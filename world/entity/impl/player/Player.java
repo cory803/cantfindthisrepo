0741ec03728cdf990ef1b0a2b75bedb53f945dd5
high105 <tannerbadalato13@gmail.com>
2016-01-06 20:58:00 -0700

    Crossbow speed lowered

--- world/entity/impl/player/Player.java
+++ world/entity/impl/player/Player.java
@@ -177,7 +177,7 @@ public class Player extends Character {
 			speed -= 2;
 		}
 		if(fightType == FightType.CROSSBOW_RAPID ) {
-			speed--;
+			speed -= (double)0.1;
 		}
 		if (fightType == FightType.LONGBOW_RAPID || weaponId == 6522 && fightType == FightType.KNIFE_RAPID || weapon.contains("rapier")) {
 			if(weaponId != 11235 && weaponId != 21016 && weaponId != 21017 && weaponId != 21018 && weaponId != 21019 && weaponId != 21020 && weaponId != 21021 && weaponId != 21022 && weaponId != 21023) {
