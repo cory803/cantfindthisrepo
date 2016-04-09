84b06ef8fd3de27392bb258255f6d98c08f21b7d
Vados-RS <vados.rs@outlook.com>
2016-04-09 13:03:05 +0100

    Fixed coin pouch bug.

--- src/com/ikov/model/input/impl/SellShards.java
+++ src/com/ikov/model/input/impl/SellShards.java
@@ -5,12 +5,17 @@ import com.ikov.model.input.EnterAmount;
 import com.ikov.util.Misc;
 import com.ikov.world.entity.impl.player.Player;
 
+import java.text.NumberFormat;
+import java.util.Locale;
+
 public class SellShards extends EnterAmount {
 
 	@Override
 	public void handleAmount(Player player, int amount) {
+		Locale locale = new Locale("en", "US");
+		NumberFormat currencyFormatter = NumberFormat.getInstance(locale);
 		boolean use_pouch = false;
-		if(amount > 85880000) {
+		if(amount >= 85899345) {
 			use_pouch = true;
 		}
 			
@@ -26,9 +31,9 @@ public class SellShards extends EnterAmount {
 				player.getInventory().delete(18016, amount);
 				for(int i = 0; i < 25; i++) {
 					player.setMoneyInPouch((player.getMoneyInPouch() + amount));
+					player.getPacketSender().sendString(8135, "" + player.getMoneyInPouch());
+					player.getPacketSender().sendMessage("Your coins have been added to your coin pouch.");
 				}
-				player.getPacketSender().sendMessage("You've sold "+amount+" Spirit Shards for coins into your money pouch.");
-				player.getPacketSender().sendString(8135, ""+player.getMoneyInPouch());
 			} else {
 				int rew = ItemDefinition.forId(18016).getValue() * amount;
 				player.getInventory().delete(18016, (int) amount);
