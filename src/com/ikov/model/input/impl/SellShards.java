371dd227b9b04595d21ede32e73f72cbf3e7ebb6
JonathanSirens <jonathan.sirens@gmail.com>
2016-04-09 22:58:29 -0700

    Selling Spirit Shards Redone

--- src/com/ikov/model/input/impl/SellShards.java
+++ src/com/ikov/model/input/impl/SellShards.java
@@ -8,38 +8,43 @@ import com.ikov.world.entity.impl.player.Player;
 import java.text.NumberFormat;
 import java.util.Locale;
 
+/**
+* Handles selling spirit shards
+* @Jonathan Sirens
+**/
+
 public class SellShards extends EnterAmount {
 
 	@Override
 	public void handleAmount(Player player, int amount) {
-		Locale locale = new Locale("en", "US");
-		NumberFormat currencyFormatter = NumberFormat.getInstance(locale);
-		boolean use_pouch = false;
-		if(amount >= 85899345) {
-			use_pouch = true;
+		//Sets the amount that you actually have in your inventory of Spirit shards
+		if(player.getInventory().getAmount(18016) < amount) {
+			amount = player.getInventory().getAmount(18016);
 		}
-			
-		player.getPacketSender().sendInterfaceRemoval();
-		
-		int shards = player.getInventory().getAmount(18016);
-		if(amount > shards)
-			amount = shards;
-		if(amount == 0) {
+		//Tells if the amount of Spirit shards in your inventory is 0 or duplication amount under negative
+		if(amount <= 0) {
+			player.getPacketSender().sendMessage("You do not have this amount of Spirit shards!");
 			return;
+		}
+		if((long) player.getInventory().getAmount(995) + ((long) amount * ItemDefinition.forId(18016).getValue()) > Integer.MAX_VALUE) {
+			player.getPacketSender().sendInterfaceRemoval();
+			
+			//If the inventory doesn't have enough room, sell shards to pouch
+			player.setMoneyInPouch((player.getMoneyInPouch() + ((long) amount * ItemDefinition.forId(18016).getValue())));
+			player.getInventory().delete(18016, amount);
+			player.getPacketSender().sendMessage("You have sold <col=ff0000>"+Misc.formatAmount(amount)+"</col> Spirit shards for <col=ff0000>"+Misc.formatAmount((long) amount * ItemDefinition.forId(18016).getValue())+"</col> coins to your money pouch.");
+			player.getPacketSender().sendString(8135, ""+player.getMoneyInPouch());
 		} else {
-			if(use_pouch) {
-				player.getInventory().delete(18016, amount);
-				for(int i = 0; i < 25; i++) {
-					player.setMoneyInPouch((player.getMoneyInPouch() + amount));
-					player.getPacketSender().sendString(8135, "" + player.getMoneyInPouch());
-					player.getPacketSender().sendMessage("Your coins have been added to your coin pouch.");
-				}
-			} else {
-				int rew = ItemDefinition.forId(18016).getValue() * amount;
-				player.getInventory().delete(18016, (int) amount);
-				player.getInventory().add(995, rew);
-				player.getPacketSender().sendMessage("You've sold "+amount+" Spirit Shards for "+Misc.insertCommasToNumber(""+(int)rew)+" coins.");
+			if(!player.getInventory().hasRoomFor(995, amount)) {
+				player.getPacketSender().sendMessage("You do not have enough inventory space to hold <col=ff0000>"+Misc.formatAmount(amount)+"</col> coins.");
+				return;
 			}
+			player.getPacketSender().sendInterfaceRemoval();
+			
+			//If inventory can hold the coins, sell the spirit shards for coins to inventory
+			player.getInventory().delete(18016, amount);
+			player.getInventory().add(995, amount * ItemDefinition.forId(18016).getValue());
+			player.getPacketSender().sendMessage("You have sold <col=ff0000>"+Misc.formatAmount(amount)+"</col> Spirit shards for <col=ff0000>"+Misc.formatAmount((long) amount * ItemDefinition.forId(18016).getValue())+"</col> coins to your inventory.");
 		}
 	}
 
