8a8652756c8213b7d4a1d49c74c334bb7218482c
JonathanSirens <jonathan.sirens@gmail.com>
2016-04-23 11:02:13 -0700

    misc

--- BootServer.bat
+++ BootServer.bat
@@ -1,5 +1,5 @@
 @echo off
 title RuneLive
 COLOR 2F
-"C:/Program Files (x86)/java/jdk1.8.0_25/bin/java.exe" -server -Xmx1024m -cp bin;lib/* com.runelive.GameServer
+"C:/Program Files (x86)/java/jdk1.8.0_25/bin/java.exe" -server -Xmx512m -cp bin;lib/* com.runelive.GameServer
 pause
