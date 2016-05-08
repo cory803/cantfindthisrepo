bce1235198278f4be91be215bdad9d1c063c0a15
JonathanSirens <jonathan.sirens@gmail.com>
2016-05-08 08:44:47 -0700

    Killstreak fix

--- src/com/runelive/world/content/combat/pvp/Killstreak.java
+++ src/com/runelive/world/content/combat/pvp/Killstreak.java
@@ -39,27 +39,22 @@ public class Killstreak {
 				}
 			}
 		}
-		switch(other.getPlayerKillingAttributes().getPlayerKillStreak()) {
-			case 10:
-				pkPoints += 25;
-				World.sendWildernessMessage("<icon=2><shad=ff0000>"+other.getUsername()+"'s killstreak of "+other.getPlayerKillingAttributes().getPlayerKillStreak()+" has been ended by "+player.getUsername()+"!");
-				break;
-			case 25:
-				pkPoints += 50;
-				World.sendWildernessMessage("<icon=2><shad=ff0000>"+other.getUsername()+"'s killstreak of "+other.getPlayerKillingAttributes().getPlayerKillStreak()+" has been ended by "+player.getUsername()+"!");
-				break;
-			case 50:
-				pkPoints += 125;
-				World.sendMessage("<icon=2><shad=ff0000>"+other.getUsername()+"'s killstreak of "+other.getPlayerKillingAttributes().getPlayerKillStreak()+" has been ended by "+player.getUsername()+"!");
-				break;
-			case 75:
-				pkPoints += 250;
-				World.sendMessage("<icon=2><shad=ff0000>"+other.getUsername()+"'s killstreak of "+other.getPlayerKillingAttributes().getPlayerKillStreak()+" has been ended by "+player.getUsername()+"!");
-				break;
-			case 100:
-				pkPoints += 500;
-				World.sendMessage("<icon=2><shad=ff0000>"+other.getUsername()+"'s killstreak of "+other.getPlayerKillingAttributes().getPlayerKillStreak()+" has been ended by "+player.getUsername()+"!");
-				break;
+		int otherKillstreak = other.getPlayerKillingAttributes().getPlayerKillStreak();
+		if(otherKillstreak >= 10 && otherKillstreak < 25) {
+			pkPoints += 25;
+			World.sendWildernessMessage("<icon=2><shad=ff0000>"+other.getUsername()+"'s killstreak of "+other.getPlayerKillingAttributes().getPlayerKillStreak()+" has been ended by "+player.getUsername()+"!");
+		} else if(otherKillstreak >= 25 && otherKillstreak < 50) {
+			pkPoints += 50;
+			World.sendWildernessMessage("<icon=2><shad=ff0000>"+other.getUsername()+"'s killstreak of "+other.getPlayerKillingAttributes().getPlayerKillStreak()+" has been ended by "+player.getUsername()+"!");
+		} else if(otherKillstreak >= 50 && otherKillstreak < 75) {
+			pkPoints += 125;
+			World.sendMessage("<icon=2><shad=ff0000>"+other.getUsername()+"'s killstreak of "+other.getPlayerKillingAttributes().getPlayerKillStreak()+" has been ended by "+player.getUsername()+"!");
+		} else if(otherKillstreak >= 75 && otherKillstreak < 100) {
+			pkPoints += 250;
+			World.sendMessage("<icon=2><shad=ff0000>"+other.getUsername()+"'s killstreak of "+other.getPlayerKillingAttributes().getPlayerKillStreak()+" has been ended by "+player.getUsername()+"!");
+		} else if(otherKillstreak >= 100) {
+			pkPoints += 500;
+			World.sendMessage("<icon=2><shad=ff0000>"+other.getUsername()+"'s killstreak of "+other.getPlayerKillingAttributes().getPlayerKillStreak()+" has been ended by "+player.getUsername()+"!");
 		}
 		switch(killstreak) {
 			case 10:
