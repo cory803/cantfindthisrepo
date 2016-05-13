c1665cd2625e9637d98c218f439e53abb1b1c691
high105 <tannerbadalato13@gmail.com>
2016-05-13 14:55:55 -0700

    Mining Animations Updated

--- src/com/runelive/world/content/skill/impl/mining/MiningData.java
+++ src/com/runelive/world/content/skill/impl/mining/MiningData.java
@@ -10,9 +10,9 @@ public class MiningData {
 
   public static enum Pickaxe {
 
-    Bronze(1265, 1, 625, 1.0), Iron(1267, 1, 626, 1.05), Steel(1269, 6, 627, 1.1), Mithril(1273, 21,
-        628, 1.2), Adamant(1271, 31, 629, 1.25), Rune(1275, 41, 624, 1.3), Dragon(15259, 61, 12188,
-            1.50), Dragon_G(20786, 61, 12188, 1.65), Adze(13661, 80, 10226, 1.60);
+    Bronze(1265, 1, 6753, 1.0), Iron(1267, 1, 6754, 1.05), Steel(1269, 6, 6755, 1.1), Mithril(1273, 21,
+        6757, 1.2), Adamant(1271, 31, 6756, 1.25), Rune(1275, 41, 6752, 1.3), Dragon(15259, 61, 12188,
+            1.50), Dragon_G(20786, 61, 12190, 1.65), Adze(13661, 80, 10226, 1.60);
 
     private int id, req, anim;
     private double speed;
