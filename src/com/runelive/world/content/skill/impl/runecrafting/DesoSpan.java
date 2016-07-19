069dd69ce933f487d65c6b1c7fbcf994dd282c9c
Dave <vados.rs@outlook.com>
2016-07-19 14:32:12 +0100

    Deso RuneSpan fix.

--- src/com/runelive/world/content/skill/impl/runecrafting/DesoSpan.java
+++ src/com/runelive/world/content/skill/impl/runecrafting/DesoSpan.java
@@ -91,8 +91,7 @@ public class DesoSpan {
 					player.performGraphic(new Graphic(energyType.playerGraphic, GraphicHeight.HIGH));
 					n.performGraphic(new Graphic(energyType.npcGraphic, GraphicHeight.HIGH));
 					n.dealDamage(new Hit(Misc.getRandom(12), Hitmask.RED, CombatIcon.MAGIC));
-					if (Misc.getRandom(Skillcape_Data.RUNECRAFTING.isWearingCape(player)
-							|| Skillcape_Data.MASTER_RUNECRAFTING.isWearingCape(player) ? 50 : 30) <= 10) {
+					if (Skillcape_Data.RUNECRAFTING.isWearingCape(player) || (Skillcape_Data.MASTER_RUNECRAFTING.isWearingCape(player) ? 50 : 30) <= 10) {
 						player.dealDamage(new Hit(1 + Misc.getRandom(48), Hitmask.RED, CombatIcon.DEFLECT));
 						player.getPacketSender()
 								.sendMessage("You accidently attempt to siphon too much energy, and get hurt.");
