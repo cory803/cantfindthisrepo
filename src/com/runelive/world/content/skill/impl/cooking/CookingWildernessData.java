f1a53c631a6cae64670efe0f9772525bb726ea9e
Dave <vados.rs@outlook.com>
2016-07-20 17:54:01 +0100

    Fishing exp

--- src/com/runelive/world/content/skill/impl/cooking/CookingWildernessData.java
+++ src/com/runelive/world/content/skill/impl/cooking/CookingWildernessData.java
@@ -13,65 +13,13 @@ import com.runelive.world.entity.impl.player.Player;
  */
 public enum CookingWildernessData {
 
-	SHRIMP(317, 315, 7954, 1, 1500, 33, "shrimp"), ANCHOVIES(321, 319, 323, 1, 1500, 34, "anchovies"), TROUT(335, 333,
-			343, 15, 3500, 50,
-			"trout"), COD(341, 339, 343, 18, 3750, 54, "cod"), SALMON(331, 329, 343, 25, 3750, 58, "salmon"), TUNA(359,
-					361, 367, 30, 5000, 58, "tuna"), LOBSTER(377, 379, 381, 40, 6000, 74, "lobster"), BASS(363, 365,
-							367, 40, 8000, 75,
-							"bass"), SWORDFISH(371, 373, 375, 45, 7000, 86, "swordfish"), MONKFISH(7944, 7946, 7948, 62,
-									7500, 91, "monkfish"), SHARK(383, 385, 387, 80, 10500, 94, "shark"), SEA_TURTLE(395,
-											397, 399, 82, 10550, 105, "sea turtle"), MANTA_RAY(389, 391, 393, 91, 10550,
-													99, "manta ray"), ROCKTAIL(15270, 15272, 15274, 92, 11250, 99,
-															"rocktail"), CAVEFISH(15264, 15266, 15268, 88, 8560, 94,
-																	"cavefish"), KARAMBWAN(3142, 3144, 3148, 30, 9500,
-																			90, "karambwan"), HEIM_CRAB(17797, 18159,
-																					18179, 5, 50, 40,
-																					"heim crab"), RED_EYE(17799, 18161,
-																							18181, 10, 75, 45,
-																							"red-eye"), DUSK_EEL(17801,
-																									18163, 18183, 12,
-																									82, 47,
-																									"dusk eel"), GIANT_FLATFISH(
-																											17803,
-																											18165,
-																											18185, 15,
-																											90, 50,
-																											"giant flatfish"), SHORT_FINNED_EEL(
-																													17805,
-																													18167,
-																													18187,
-																													18,
-																													114,
-																													54,
-																													"short-finned eel"), WEB_SNIPPER(
-																															17807,
-																															18169,
-																															18189,
-																															30,
-																															211,
-																															60,
-																															"web snipper"), BOULDABASS(
-																																	17809,
-																																	18171,
-																																	18191,
-																																	40,
-																																	288,
-																																	75,
-																																	"bouldabass"), SALVE_EEL(
-																																			17811,
-																																			18173,
-																																			18193,
-																																			60,
-																																			415,
-																																			81,
-																																			"salve eel"), BLUE_CRAB(
-																																					17813,
-																																					18175,
-																																					18195,
-																																					75,
-																																					1066,
-																																					92,
-																																					"blue crab");
+	SHRIMP(317, 315, 7954, 1, 30, 33, "shrimp"), ANCHOVIES(321, 319, 323, 1, 30, 34, "anchovies"), TROUT(335, 333, 343, 15, 70, 50, "trout"),
+	COD(341, 339, 343, 18, 75, 54, "cod"), SALMON(331, 329, 343, 25, 90, 58, "salmon"), TUNA(359, 361, 367, 30, 100, 58, "tuna"),
+	LOBSTER(377, 379, 381, 40, 120, 74, "lobster"), BASS(363, 365, 367, 40, 8000, 75, "bass"), SWORDFISH(371, 373, 375, 45, 140, 86, "swordfish"),
+	MONKFISH(7944, 7946, 7948, 62, 150, 92, "monkfish"), SHARK(383, 385, 387, 80, 210, 99, "shark"),
+	SEA_TURTLE(395, 397, 399, 82, 212, 105, "sea turtle"), MANTA_RAY(389, 391, 393, 91, 216, 105, "manta ray"),
+	ROCKTAIL(15270, 15272, 15274, 92, 250, 106, "rocktail"), CAVEFISH(15264, 15266, 15268, 88, 214, 101, "cavefish"),
+	KARAMBWAN(3142, 3144, 3148, 30, 190, 90, "karambwan");
 
 	int rawItem, cookedItem, burntItem, levelReq, xp, stopBurn;
 	String name;
