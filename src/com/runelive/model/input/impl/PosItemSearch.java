2b0a5c3b4c51d98836dfb2e3e1f6aee7af29b577
X <x@gmail.com>
2016-08-11 01:28:25 -0600

    -Item searching fixed I think, needs testing

--- src/com/runelive/model/input/impl/PosItemSearch.java
+++ src/com/runelive/model/input/impl/PosItemSearch.java
@@ -1,31 +1,115 @@
 package com.runelive.model.input.impl;
 
-import java.text.NumberFormat;
-import java.util.Comparator;
-import java.util.LinkedHashMap;
-import java.util.Locale;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.stream.Stream;
-
 import com.runelive.model.definitions.ItemDefinition;
 import com.runelive.model.input.Input;
 import com.runelive.world.content.PlayerPunishment;
-import com.runelive.world.content.dialogue.DialogueManager;
 import com.runelive.world.content.pos.PlayerOwnedShops;
 import com.runelive.world.content.pos.PosDetails;
 import com.runelive.world.content.pos.PosOffer;
 import com.runelive.world.content.pos.PosOffers;
 import com.runelive.world.entity.impl.player.Player;
 
+import java.text.NumberFormat;
+import java.util.Comparator;
+import java.util.LinkedHashMap;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.Stream;
+
 public class PosItemSearch extends Input {
 
+	private static final ExecutorService EXECUTOR = Executors.newCachedThreadPool();
+
+	private static final class SearchTask implements Runnable {
+
+		private final Player player;
+		private final String syntax;
+
+		public SearchTask(Player player, String syntax) {
+			this.player = player;
+			this.syntax = syntax;
+		}
+
+		@Override
+		public void run() {
+			Locale locale = new Locale("en", "US");
+			NumberFormat currencyFormatter = NumberFormat.getInstance(locale);
+			if (syntax.length() <= 1) {
+				player.getPacketSender().sendMessage("Invalid syntax entered.");
+				return;
+			}
+			int start_button = -24102;
+			int start_owner_name = 41428;
+			int start_caption = 41429;
+			int index = 0;
+
+			String itemName = syntax;
+			if (itemName.length() < 3) {
+				player.getPacketSender().sendMessage("Your search must contain atleast 3 characters.");
+				return;
+			}
+			reset(player);
+			for (PosOffers o : PlayerOwnedShops.SHOPS_ARRAYLIST) {
+				if (o == null)
+					continue;
+
+				for (int q = 0; q < o.getOffers().size(); q++) {
+					if (o.getOffers().get(q) != null && !o.getOwner().equalsIgnoreCase(player.getUsername())
+							&& !PlayerPunishment.isPlayerBanned(o.getOwner())) {
+						ItemDefinition def = ItemDefinition.forId(o.getOffers().get(q).getItemId());
+						if (def != null && def.getName().toLowerCase().contains(itemName)) {
+							Player.foundOffers.put(new PosDetails(start_button, o.getOwner(), o.getCaption()),
+									new PosOffer(o.getOffers().get(q).getItemId(), o.getOffers().get(q).getAmount(),
+											o.getOffers().get(q).getSoldAmount(), o.getOffers().get(q).getPrice()));
+							// start_button += 4;
+						}
+					}
+				}
+			}
+
+			if (Player.foundOffers.size() < 1) {
+				player.getPacketSender().sendMessage("Your search did not return any offers.");
+				return;
+			}
+
+			Player.foundOffers = sortByValue(Player.foundOffers);
+
+			for (Map.Entry<PosDetails, PosOffer> entry : Player.foundOffers.entrySet()) {
+				if (index >= 100) {
+					break;
+				}
+				PosDetails pd = entry.getKey();
+				PosOffer p = entry.getValue();
+				String item_name = ItemDefinition.forId(p.getItemId()).getName();
+				start_caption += 4;
+				start_owner_name += 4;
+				pd.setButtonId(-24102 + (4 * index));
+				player.getPacketSender().sendString(start_owner_name, pd.getOwner());
+				player.getPacketSender().sendString(start_caption,
+						"Found: " + item_name + " for " + currencyFormatter.format(p.getPrice()) + " GP");
+				index++;
+			}
+
+			for (start_caption = start_caption; start_caption < 41834; start_caption = start_caption) {
+				start_caption += 4;
+				start_owner_name += 4;
+				player.getPacketSender().sendString(start_caption, "");
+				player.getPacketSender().sendString(start_owner_name, "");
+			}
+		}
+	}
 
 	@Override
 	public void handleSyntax(Player player, String syntax) {
 
-		player.getPacketSender().sendMessage("<col=ff0000>Searching items has been disabled temporarily, this was confirmed to be");
-		player.getPacketSender().sendMessage("<col=ff0000>the source of the lag issue in the game. Enjoy no lag :)");
+//		player.getPacketSender().sendMessage("<col=ff0000>Searching items has been disabled temporarily, this was confirmed to be");
+//		player.getPacketSender().sendMessage("<col=ff0000>the source of the lag issue in the game. Enjoy no lag :)");
+
+		player.getPacketSender().sendMessage("Conducting search...");
+		EXECUTOR.submit(new SearchTask(player, syntax));
 		/*
 		Locale locale = new Locale("en", "US");
 		NumberFormat currencyFormatter = NumberFormat.getInstance(locale);
