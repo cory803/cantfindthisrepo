3fab0c01729ef43a77e80cc4abdf663c2d59022e
high105 <tannerbadalato13@gmail.com>
2016-09-12 18:46:03 -0600

    Home command = home achievement

--- src/org/scripts/kotlin/content/commands/TeleportHome.kt
+++ src/org/scripts/kotlin/content/commands/TeleportHome.kt
@@ -4,6 +4,7 @@ import com.chaos.model.Locations
 import com.chaos.model.StaffRights
 import com.chaos.model.Position
 import com.chaos.model.player.command.Command
+import com.chaos.world.content.Achievements
 import com.chaos.world.content.transportation.TeleportHandler
 import com.chaos.world.entity.impl.player.Player
 
@@ -24,5 +25,6 @@ class TeleportHome(staffRights: StaffRights) : Command(staffRights) {
         val position = Position(3212, 3428, 0)
         TeleportHandler.teleportPlayer(player, position, player.spellbook.teleportType)
         player.packetSender.sendMessage("Teleporting you home!")
+        Achievements.finishAchievement(player, Achievements.AchievementData.TELEPORT_HOME)
     }
 }
