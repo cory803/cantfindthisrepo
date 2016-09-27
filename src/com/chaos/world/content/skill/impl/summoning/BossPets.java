3764054b90ca96d54d3010b74a571bb9b25fc690
Adam <adam@chaosps.com>
2016-09-27 00:31:35 -0700

    Boss pets
    I added descriptions for what I want every benefit for each boss to
    have. I will add the benefits later on.

--- src/com/chaos/world/content/skill/impl/summoning/BossPets.java
+++ src/com/chaos/world/content/skill/impl/summoning/BossPets.java
@@ -5,50 +5,179 @@ import com.chaos.world.entity.impl.player.Player;
 
 public class BossPets {
 
+	/**
+	 *  Boss Pets
+	 *	Effects are given to the player only when used with
+	 *	@return
+	 */
+
 	public enum BossPet {
 
-		PET_CHAOS_ELEMENTAL(5907, 5907, 11995),
-		PRINCE_BLACK_DRAGON(6652, 6652, 12653),
-		PET_GENERAL_GRAARDOR(6644, 6644, 12650),
-		PET_TZTOK_JAD(5892, 5892, 13225),
-		PET_CORPOREAL_BEAST(8133, 3034, 12001),
-		PET_KREE_ARRA(6643, 6643, 12649),
-		PET_KRIL_TSUTSAROTH(6647, 6647, 12652),
-		PET_COMMANDER_ZILYANA(6646, 6646, 12651),
-		PET_DAGANNOTH_SUPREME(6626, 6626, 12643),
-		PET_DAGANNOTH_PRIME(6627, 6627, 12644),
-		PET_DAGANNOTH_REX(6641, 6641, 12645),
-		PET_FROST_DRAGON(51, 3047, 11991),
-		PET_TORMENTED_DEMON(8349, 3048, 11992),
-		PET_KALPHITE_QUEEN(6653, 6653, 12654),
-		PET_SLASH_BASH(2060, 3051, 11994),
-		PET_PHOENIX(8549, 3052, 11989),
-		PET_BANDOS_AVATAR(4540, 3053, 11988),
-		PET_NEX(13447, 3054, 11987),
-		PET_JUNGLE_STRYKEWYRM(9467, 3055, 11986),
-		PET_DESERT_STRYKEWYRM(9465, 3056, 11985),
-		PET_ICE_STRYKEWYRM(9463, 3057, 11984),
-		PET_GREEN_DRAGON(941, 3058, 11983),
-		PET_BABY_BLUE_DRAGON(52, 3059, 11982),
-		PET_BLUE_DRAGON(55, 3060, 11981),
-		PET_BLACK_DRAGON(54, 3061, 11979),
-		PET_QUEEN_WHITE_DRAGON(4000, 4001, 13000),
-		HELLPUPPY(964, 964, 13247),
-		VENENATIS(495, 495, 13177),
-		PET_SEAGULL(2707, 2707, 7887),
-		PET_ROCK(8648, 8648, 21250),
-		PET_CHINCHOMPA(9579, 9579, 21251),
-		PET_RACCOON(5559, 5559, 12487),
-		DUNGEONEERING_PET(13089, 13089, 19891),
-		CALLISTO_CUB(497, 497, 13178),
+		/**
+		 *  Chaos Elemental Jr.
+		 *	Don't get teleported away in combat
+		 *	@Chaos Elemental
+		 */
+		PET_CHAOS_ELEMENTAL(3200, 5907, 11995),
+
+		/**
+		 *  Prince Black Dragon
+		 *	Never get damaged by fire breath.
+		 *	@All Dragons
+		 */
+		PRINCE_BLACK_DRAGON(50, 6652, 12653),
+
+		/**
+		 *  General Graardor Jr.
+		 *	Require no kill count to enter Bandos.
+		 *	@Godwars
+		 */
+		PET_GENERAL_GRAARDOR(3031, 6644, 12650),
+
+		/**
+		 *  TzRek-Jad
+		 *	Trade a fire cape or tokhaar kal to a player.
+		 *	@All Players
+		 */
+		TZREK_JAD(2745, 5892, 13225),
+
+		/**
+		 *  Dark Core
+		 *	Receive a 25% defensive boost against Corporeal Beast.
+		 *	@Corporeal Beast
+		 */
+		PET_DARK_CORE(8133, 388, 12816),
+
+		/**
+		 *  Kree'arra Jr.
+		 *	Require no kill count to enter Armadyl.
+		 *	@Godwars
+		 */
+		PET_KREE_ARRA(6222, 6643, 12649),
+
+		/**
+		 *  K'ril Tsutsaroth Jr.
+		 *	Require no kill count to enter Zamorak.
+		 *	@Godwars
+		 */
+		PET_KRIL_TSUTSAROTH(6203, 6647, 12652),
+
+		/**
+		 *  Zilyana Jr.
+		 *	Require no kill count to enter Saradomin.
+		 *	@Godwars
+		 */
+		PET_ZILYANA(6247, 6646, 12651),
+
+		/**
+		 *  Dagannoth Supreme Jr.
+		 *	Gain a 10% overall damage boost.
+		 *	@Dagannoth Supreme
+		 */
+		PET_DAGANNOTH_SUPREME(2881, 6626, 12643),
+
+		/**
+		 *  Dagannoth Prime Jr.
+		 *	Gain a 10% overall damage boost.
+		 *	@Dagannoth Prime
+		 */
+		PET_DAGANNOTH_PRIME(2882, 6627, 12644),
+
+		/**
+		 *  Dagannoth Rex Jr.
+		 *	Gain a 10% overall damage boost.
+		 *	@Dagannoth Rex
+		 */
+		PET_DAGANNOTH_REX(2883, 6641, 12645),
+
+		/**
+		 *  Kalphite Princess
+		 *	Hit through combat deflection prayers.
+		 *	@Kalphite Queen
+		 */
+		KALPHITE_PRINCESS(6653, 6653, 12654),
+
+		/**
+		 *  Hellpuppy
+		 *	Gain a 10% increased drop rate.
+		 *	@Cerberus
+		 */
+		HELLPUPPY(1158, 964, 13247),
+
+		/**
+		 *  Venenatis Spiderling
+		 *	Have unlimited prayer points
+		 *	@Venenatis
+		 */
+		VENENATIS_SPIDERLING(2000, 495, 13177),
+
+		/**
+		 *  Callisto Cub
+		 *	Block 25% of all damage.
+		 *	@Callisto
+		 */
+		CALLISTO_CUB(6609, 497, 13178),
+
+		/**
+		 *  Kraken
+		 *	Find your opponent on the first whirlpool.
+		 *	@Kraken
+		 */
+		//TODO: Kraken isn't added yet
 		PET_KRAKEN(6656, 6656, 12655),
-		PET_SCORPIA(5547, 5547, 13181),
+
+		/**
+		 *  Scorpia's offspring
+		 *	Protect yourself from venom & poison.
+		 *	@Scorpia
+		 */
+		SCORPIAS_OFFSPRING(2001, 5547, 13181),
+
+		/**
+		 *  Scorpia's offspring
+		 *	Protect yourself from venom & poison.
+		 *	@Scorpia
+		 */
+		//TODO: Vetion isn't added yet
 		PET_VETION(5536, 5536, 13179),
+
+		/**
+		 *  Smoke Devil
+		 *	-Undecided-
+		 *	@return null
+		 */
 		SMOKE_DEVIL(6655, 6655, 12648),
-		BEAVER(6717, 6717, 13322),
+
+		/**
+		 *  Beaver
+		 *	Gain double the logs from woodcutting.
+		 *	Stackable with all other benefits.
+		 *	@Woodcutting
+		 */
+		BEAVER(-1, 6717, 13322),
+
+		/**
+		 *  Heron
+		 *	Catch double hunter entitys.
+		 *	Stackable with all other benefits.
+		 *	@Hunter
+		 */
 		HERON(6715, 6715, 13320),
+
+		/**
+		 *  Rock Golem
+		 *	Mine 2 ores at a time.
+		 *	Stackable with all other benefits.
+		 *	@Mining
+		 */
 		ROCK_GOLEM(6716, 6716, 13321),
 
+
+		/**
+		 *  Snakeling
+		 *	-Undecided-
+		 *	@return null
+		 */
 		ZULRAH_RANGE(2127, 2127, 12921),
 		ZULRAH_MAGE(2129, 2129, 12940),
 		ZULRAH_MELEE(2128, 2128, 12939);
