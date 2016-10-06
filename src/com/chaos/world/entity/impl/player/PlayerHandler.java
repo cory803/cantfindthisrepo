5d220b533ffedfc06928d2640e5cfaa13dff1f49
Adam <adam@chaosps.com>
2016-10-06 01:55:33 -0700

    Title system

--- src/com/chaos/world/entity/impl/player/PlayerHandler.java
+++ src/com/chaos/world/entity/impl/player/PlayerHandler.java
@@ -22,6 +22,7 @@ import com.chaos.model.container.impl.Equipment;
 import com.chaos.model.definitions.WeaponAnimations;
 import com.chaos.model.definitions.WeaponInterfaces;
 import com.chaos.model.input.impl.ChangePassword;
+import com.chaos.model.player.Titles;
 import com.chaos.net.PlayerSession;
 import com.chaos.net.SessionState;
 import com.chaos.net.login.LoginResponses;
@@ -123,7 +124,7 @@ public class PlayerHandler {
 				"Friends List (" + player.getRelations().getFriendList().size() + "/200)");
 		// Relations
 		player.getRelations().setPrivateMessageId(1).onLogin(player).updateLists(true, 1);
-
+		Titles.setTitle(player);
 		// Client configurations
 		player.getPacketSender().sendConfig(172, player.isAutoRetaliate() ? 1 : 0)
 				.sendTotalXp(player.getSkillManager().getTotalGainedExp())
