4c4ed02f501972515ab05ea8d164bfe9f89fe0a9
high105 <tannerbadalato13@gmail.com>
2016-10-06 20:00:17 -0600

    Ezone + More Tutorial

--- src/org/scripts/kotlin/content/dialog/Tutorial/StartTutorial.java
+++ src/org/scripts/kotlin/content/dialog/Tutorial/StartTutorial.java
@@ -5,8 +5,6 @@ import com.chaos.model.Flag;
 import com.chaos.model.Item;
 import com.chaos.model.Position;
 import com.chaos.model.container.impl.Equipment;
-import com.chaos.model.definitions.WeaponAnimations;
-import com.chaos.model.definitions.WeaponInterfaces;
 import com.chaos.model.options.fouroption.FourOption;
 import com.chaos.model.options.twooption.TwoOption;
 import com.chaos.model.player.GameMode;
@@ -16,14 +14,11 @@ import com.chaos.model.player.dialog.DialogMessage;
 import com.chaos.net.packet.impl.EquipPacketListener;
 import com.chaos.world.content.PlayerPanel;
 import com.chaos.world.entity.impl.player.Player;
-import com.chaos.world.entity.updating.PlayerUpdating;
-import org.scripts.kotlin.content.dialog.BankPin.BankPinDial;
 import org.scripts.kotlin.content.dialog.BankPin.BankPinTut;
 
 public class StartTutorial extends Dialog {
 
     public Dialog dialog = this;
-    Player p;
     public StartTutorial(Player player) {
         super(player);
         setEndState(7);
@@ -174,16 +169,16 @@ public class StartTutorial extends Dialog {
                     }
                 });
             case 5:
-//                p.getPacketSender().sendCameraAngle(200,120, 2, 1, 103);
-                p.moveTo(new Position(3182, 3676, 3));
+                getPlayer().getPacketSender().sendCameraAngle(180,120, 2, 1, 103);
+                getPlayer().moveTo(new Position(3182, 3676, 3));
                 return Dialog.createNpc(DialogHandler.HAPPY, "We have an active Bounty Hunter with Revenants and Scorpia. There are also some resourace areas for skilling located inside the crator.");
             case 6:
                 return Dialog.createNpc(DialogHandler.HAPPY, "There are specific areas on the teleports that designate areas just for skilling. It makes it easier to interact with other skillers!");
             case 7:
-                p.setNewPlayer(false);
-                p.setPlayerLocked(false);
-                p.moveTo(new Position(3333, 3333, 0));
-                p.setDirection(Direction.NORTH);
+                getPlayer().setNewPlayer(false);
+                getPlayer().setPlayerLocked(false);
+                getPlayer().moveTo(new Position(3333, 3333, 0));
+                getPlayer().setDirection(Direction.NORTH);
                 return Dialog.createNpc(DialogHandler.HAPPY, "You will be placed in the global 'Chaos' clan chat and near a bank in the tutorial.");
         }
         return null;
