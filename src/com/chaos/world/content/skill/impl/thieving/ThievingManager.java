4b64bbe03af04da72066589b8f43adcb1103fa6a
high105 <tannerbadalato13@gmail.com>
2016-10-08 15:56:46 -0600

    Revert "Adjusting xp for pickpocketing"
    This reverts commit 2bdab9ef2ea2160cf8fce91ec9aabf7f2c2080f3.

--- src/com/chaos/world/content/skill/impl/thieving/ThievingManager.java
+++ src/com/chaos/world/content/skill/impl/thieving/ThievingManager.java
@@ -15,21 +15,21 @@ public class ThievingManager {
     private enum MobData {
 
 
-        MAN(new int[] {1, 2, 3, 4}, 1, new int[] {995}, 30, 2),
-        FARMER(new int[] {7}, 10, new int[] {995, 313, 314}, 15, 4.5),
-        HAM_FEMALE(new int[] {1715}, 15, new int[] {4298, 4300, 4302, 4304, 4308, 4310}, 1, 5.5),
-        HAM_MALE(new int[] {1714}, 20, new int[] {4298, 4300, 4302, 4304, 4308, 4310}, 1, 9.5),
-        ALKHARID_WARRIOR(new int[] {18}, 25, new int[] {995}, 100, 13),
-        MASTER_FARMER(new int[] {2234}, 38, new int[] {5291, 5291, 5291, 5293, 5293, 5294, 5294, 5294, 5294, 5295, 5296, 5297, 5298, 5300, 5303, 5304, 5291, 5291, 5291, 5291, 5293, 5294, 5294, 5291, 5291, 5292, 5292, 5292, 5315, 5314, 5316}, Misc.random(1, 4), 13),
-        ARDOUGNE_KNIGHT(new int[] {26, 23}, 55, new int[] {995}, Misc.getRandom(100), 24.3),
-        BANDIT(new int[] {1880}, 53, new int[] {995}, Misc.getRandom(5000), 29.5),
-        FREMENNIK_CITZEN(new int[] {1307, 1305, 1306, 1311, 1310, 1308, 1314}, 45, new int[] {995}, Misc.getRandom(200), 35),
-        GUARD(new int[] {9, 32}, 40, new int[] {995}, 150, 36.8),
-        HERO(new int[] {21}, 80, new int[] {995}, Misc.getRandom(800), 39),
-        PALADIN(new int[] {20}, 70, new int[] {995}, Misc.getRandom(500), 51.75),
+        MAN(new int[] {1, 2, 3, 4}, 1, new int[] {995}, 30, 8),
+        FARMER(new int[] {7}, 10, new int[] {995, 313, 314}, 15, 14.5),
+        HAM_FEMALE(new int[] {1715}, 15, new int[] {4298, 4300, 4302, 4304, 4308, 4310}, 1, 18.5),
+        HAM_MALE(new int[] {1714}, 20, new int[] {4298, 4300, 4302, 4304, 4308, 4310}, 1, 22.5),
+        ALKHARID_WARRIOR(new int[] {18}, 25, new int[] {995}, 100, 26),
+        MASTER_FARMER(new int[] {2234}, 38, new int[] {5291, 5291, 5291, 5293, 5293, 5294, 5294, 5294, 5294, 5295, 5296, 5297, 5298, 5300, 5303, 5304, 5291, 5291, 5291, 5291, 5293, 5294, 5294, 5291, 5291, 5292, 5292, 5292, 5315, 5314, 5316}, Misc.random(1, 4), 43),
+        ARDOUGNE_KNIGHT(new int[] {26, 23}, 55, new int[] {995}, Misc.getRandom(100), 84.3),
+        BANDIT(new int[] {1880}, 53, new int[] {995}, Misc.getRandom(5000), 79.5),
+        FREMENNIK_CITZEN(new int[] {1307, 1305, 1306, 1311, 1310, 1308, 1314}, 45, new int[] {995}, Misc.getRandom(200), 65),
+        GUARD(new int[] {9, 32}, 40, new int[] {995}, 150, 46.8),
+        HERO(new int[] {21}, 80, new int[] {995}, Misc.getRandom(800), 275),
+        PALADIN(new int[] {20}, 70, new int[] {995}, Misc.getRandom(500), 151.75),
         VILLAGER(new int[] {1888}, 45, new int[] {995}, Misc.getRandom(2000), 65),
-        MASTER_FISHER(new int[] {308}, 72, new int[] {25, 313, 378, 372, 384, 15271, 3143}, Misc.getRandom(5), 65),
-        WATCHMAN(new int[] {34}, 65, new int[] {995}, Misc.getRandom(50), 65);
+        MASTER_FISHER(new int[] {308}, 72, new int[] {25, 313, 378, 372, 384, 15271, 3143}, Misc.getRandom(5), 165),
+        WATCHMAN(new int[] {34}, 65, new int[] {995}, Misc.getRandom(50), 138);
 
         private final int[] items;
         private final int[] npcId;
