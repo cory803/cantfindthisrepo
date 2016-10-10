405fbd1395c609f9cd23a4d4cc8215de3d2f172d
Adam <adam@chaosps.com>
2016-10-09 23:22:19 -0700

    fishign shit

--- src/com/chaos/world/content/skill/impl/fishing/Fishing.java
+++ src/com/chaos/world/content/skill/impl/fishing/Fishing.java
@@ -57,9 +57,9 @@ public class Fishing {
 
 		KARAM(2859, new int[] { 3142 }, 305, -1, new int[] { 65 }, false, new int[] { 105 }, 621),
 
-		RESOURCE_ROCKTAIL(321, new int[] { 15271, 15270 }, 309, 25, new int[] { 89, 89 }, false, new int[] { 480 }, 623),
+		RESOURCE_ROCKTAIL(321, new int[] { 15271, 15270 }, 309, 25, new int[] { 89, 89 }, false, new int[] { 480, 480 }, 623),
 
-		RESOURCE_KARAM(322, new int[] { 3143, 3142 }, 305, -1, new int[] { 65, 65 }, false, new int[] { 225 }, 621);
+		RESOURCE_KARAM(322, new int[] { 3143, 3142 }, 305, -1, new int[] { 65, 65 }, false, new int[] { 225, 225 }, 621);
 
 		int npcId, equipment, bait, anim;
 		int[] rawFish, fishingReqs, xp;
